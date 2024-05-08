9b07532c23768392f347599d14f03e0d856bbe78
pacristi <piero.cristi@uc.cl>
2024-05-08 16:16:50 -0400

    feat/database connection

---
+++ src/database/db.ts
@@ -0,0 +1,12 @@
+// import { Sequelize } from 'sequelize';
+
+// const sequelize = new Sequelize({
+//   dialect: 'postgres',
+//   database: process.env.DB_NAME,
+//   username: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   host: process.env.DB_HOST,
+//   port: parseInt(process.env.DB_PORT || '5432'), // handle default port
+// });
+
+// export default sequelize;
