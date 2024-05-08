a1449e11013ee681e4ba51fb379acca29ab2d29d
pacristi <piero.cristi@uc.cl>
2024-05-08 17:12:22 -0400

    feat/publications router and controller

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { env } from "process";
+import publicationsRouter from "./routes/publications";
 
 
 const app = express();
@@ -8,6 +9,8 @@ const port = env["PORT"] || 8080;
 app.get('/', (_req, res) => {
     res.send('Hello World!');
 });
+
+app.use('/publications', publicationsRouter);
 
 app.listen(port, () => {
     console.log(`Listening at http://127.0.0.1:${port}`);
--- src/routes/publications.ts
+++ src/routes/publications.ts
@@ -1,21 +1,21 @@
-// import express from 'express';
-// import { getAllPublications, getPublicationById, createPublication, updatePublication, deletePublication } from '../controllers/publications.ts';
+import express from 'express';
+import { getAllPublications, getPublicationById, createPublication, updatePublication, deletePublication } from '../controllers/publications';
 
-// const router = express.Router();
+const router = express.Router();
 
-// // GET /publications - Get all publications
-// router.get('/', getAllPublications);
+// GET /publications - Get all publications
+router.get('/', getAllPublications);
 
-// // GET /publications/:id - Get a publication by its ID
-// router.get('/:id', getPublicationById);
+// GET /publications/:id - Get a publication by its ID
+router.get('/:id', getPublicationById);
 
-// // POST /publications - Create a new publication
-// router.post('/', createPublication);
+// POST /publications - Create a new publication
+router.post('/', createPublication);
 
-// // PUT /publications/:id - Update a publication
-// router.put('/:id', updatePublication);
+// PUT /publications/:id - Update a publication
+router.put('/:id', updatePublication);
 
-// // DELETE /publications/:id - Delete a publication
-// router.delete('/:id', deletePublication);
+// DELETE /publications/:id - Delete a publication
+router.delete('/:id', deletePublication);
 
-// export default router;
+export default router;
