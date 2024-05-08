0b8d12c0a66740ac908756192cb62010fc0933ee
pacristi <piero.cristi@uc.cl>
2024-05-08 16:16:25 -0400

    feat/publications controller

---
+++ src/controllers/publications.ts
@@ -0,0 +1,38 @@
+// import { Request, Response } from 'express';
+
+// // Define your controller methods here
+// export const getAllPublications = (req: Request, res: Response) => {
+//     // dummy response
+//     res.json({ message: [
+//         {
+//             id: 1,
+//             title: 'Publication 1',
+//             content: 'This is the content of publication 1'
+//         },
+//         {
+//             id: 2,
+//             title: 'Publication 2',
+//             content: 'This is the content of publication 2'
+//         }
+//     ] });
+// }
+
+// export const getPublicationById = (req: Request, res: Response) => {
+//     // Logic to fetch a publication by its ID from the database
+//     // Return the publication as a response
+// };
+
+// export const createPublication = (req: Request, res: Response) => {
+//     // Logic to create a new publication in the database
+//     // Return the created publication as a response
+// };
+
+// export const updatePublication = (req: Request, res: Response) => {
+//     // Logic to update a publication in the database
+//     // Return the updated publication as a response
+// };
+
+// export const deletePublication = (req: Request, res: Response) => {
+//     // Logic to delete a publication from the database
+//     // Return a success message as a response
+// };
