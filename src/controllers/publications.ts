492e5faa8ee6b856bc5ad86beb088bd278f62bf6
pacristi <piero.cristi@uc.cl>
2024-05-09 18:43:58 -0400

    feat(controller): dummy data para el front

--- src/controllers/publications.ts
+++ src/controllers/publications.ts
@@ -3,20 +3,22 @@ import { Request, Response } from 'express';
 // Define your controller methods here
 export const getAllPublications = (req: Request, res: Response) => {
     // dummy response
-    res.json({ message: [
+    res.json({ data: [
         {
             id: 1,
             title: 'Publication 1',
             price: 22000,
             author: "John Doe",
-            description: "Description of publication 1"
+            description: "Description of publication 1",
+            image: "https://via.placeholder.com/150"
         },
         {
             id: 1,
             title: 'Publication 2',
             price: 15000,
             author: "John Doe",
-            description: "Description of publication 2"
+            description: "Description of publication 2",
+            image: "https://via.placeholder.com/150"
         }
     ] });
 }
@@ -24,6 +26,15 @@ export const getAllPublications = (req: Request, res: Response) => {
 export const getPublicationById = (req: Request, res: Response) => {
     // Logic to fetch a publication by its ID from the database
     // Return the publication as a response
+    // dummy response
+    res.json({ data: {
+        id: 1,
+        title: 'Aquí empieza todo',
+        price: 15000,
+        author: "Jennifer Niven",
+        description: "También se intercambia por\n\n - Libros de ciencia ficción\n - Otros libros de Jeniffer Niven\n - Dune parte 1",
+        image: "https://laesquinadobladablog.wordpress.com/wp-content/uploads/2017/07/img_20170720_1838111.jpg?w=840"
+    } });
 };
 
 export const createPublication = (req: Request, res: Response) => {
