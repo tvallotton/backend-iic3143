f361bf5fbd0ef01d6e2fe0657871598404d83246
Tomás Vallotton <tvallotton@uc.cl>
2024-05-13 15:56:43 -0400

    feat: setup prisma

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import { env } from "process";
 
+console.log(env);
+
 
 const app = express();
 const port = env["PORT"] || 8080;
 
 app.get('/', (_req, res) => {
-    res.send('Hello World!');
+    res.send('Hello Worlds!');
 });
 
 app.listen(port, () => {
