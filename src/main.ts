6fffbf0bcbe3a5141d8366e5c9eb62d9715899f7
pacristi <piero.cristi@uc.cl>
2024-05-16 20:27:43 -0400

    chore: Remove unused import statements in main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,26 +3,16 @@ import cors from "cors";
 import { env } from "process";
 import publicationsRouter from "./publications/routes";
 import user from "./user/controller";
-<<<<<<< HEAD
-=======
-import cors from "cors";
-
-
->>>>>>> origin/develop
 
 const app = express();
 const port = env["PORT"] || 8080;
 
-<<<<<<< HEAD
 const corsOptions = {
     origin: "http://localhost:5173"
 };
 
 app.use(cors(corsOptions));
 
-=======
-app.use(cors());
->>>>>>> origin/develop
 app.use("/", express.json());
 app.use("/user", user);
 app.use('/publications', publicationsRouter);
