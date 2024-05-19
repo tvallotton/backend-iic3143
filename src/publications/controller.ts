9e3bd0a9ea0245b9969aa4f0bf02578ff2d59cc9
pacristi <piero.cristi@uc.cl>
2024-05-19 13:05:29 -0400

    feat(publications): Update createPublication and updatePublication endpoints

--- src/publications/controller.ts
+++ src/publications/controller.ts
@@ -57,13 +57,13 @@ export const createPublication = (req: Request, res: Response) => {
     token = token.replace("Bearer ", "");
     const { id } = jwt.verify(token, JWT_SECRET, {}) as { id: number; };
 
-    const { 
-        title, 
-        author, 
+    const {
+        title,
+        author,
         language,
         genre,
         state,
-        description, 
+        description,
         type,
         price,
         image,
@@ -94,19 +94,81 @@ export const createPublication = (req: Request, res: Response) => {
     });
 };
 
-export const autocompleteData = (req: Request, res: Response) => {
-    return res.json({ 
+export const autocompleteData = (_req: Request, res: Response) => {
+    return res.json({
         author: "J.K. Rowling",
         description: "Harry is a little boy wizard bla bla bla"
      });
 }
 
-export const updatePublication = (req: Request, res: Response) => {
-    // Logic to update a publication in the database
-    // Return the updated publication as a response
+export const updatePublication = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const {description, state, type, language, price } = req.body;
+    const updateData = {
+        description,
+        state,
+        type,
+        language,
+        price,
+    };
+
+    var token = req.headers.authorization;
+    if (!token) {
+        return res.status(401).json({ error: "No token provided" });
+    }
+    token = token.replace("Bearer ", "");
+    const { id: userId } = jwt.verify(token, JWT_SECRET, {}) as { id: number; };
+
+    try {
+        const publication = await prisma.publication.findUnique({
+            where: { id: Number(id) },
+        });
+
+        if (!publication) {
+            return res.status(404).json({ error: "Publication doesn't exist" });
+        }
+
+        if (publication.ownerId !== userId) {
+            return res.status(403).json({ error: "You don't have permission to update this publication" });
+        }
+
+        const updatedPublication = await prisma.publication.update({
+            where: { id: Number(id) },
+            data: updateData,
+        });
+
+        res.json(updatedPublication);
+    } catch (error: any) {
+        console.log(error)
+        res.status(500).json({ error: error.message });
+    }
 };
 
-export const deletePublication = (req: Request, res: Response) => {
-    // Logic to delete a publication from the database
-    // Return a success message as a response
+export const deletePublication = async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    var token = req.headers.authorization;
+    if (!token) {
+        return res.status(401).json({ error: "No token provided" });
+    }
+    token = token.replace("Bearer ", "");
+    const { id: userId } = jwt.verify(token, JWT_SECRET, {}) as { id: number; };
+
+    try {
+        const publication = await prisma.publication.findUnique({
+            where: { id: Number(id) },
+        });
+
+        if (!publication || publication.ownerId !== userId) {
+            return res.status(403).json({ error: "You don't have permission to delete this publication" });
+        }
+
+        await prisma.publication.delete({
+            where: { id: Number(id) },
+        });
+
+        res.json({ message: "Publication deleted successfully" });
+    } catch (error: any) {
+        res.status(500).json({ error: error.message });
+    }
 };
