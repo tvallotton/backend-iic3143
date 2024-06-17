b56e8099ab86d8325b55f98f1aa5efb8242ae1f8
Jose Marchant <jose_marchant@uc.cl>
2024-06-17 16:00:32 -0400

    feat: add user middleware where needed + format

--- src/reviews/routes.ts
+++ src/reviews/routes.ts
@@ -1,24 +1,32 @@
-import express from 'express';
-import { getAllReviews, getReviewById, createReview, getReviewsReceived, getReviewsGiven, getUserRating } from './controller.js';
+import express from "express";
+import {
+  getAllReviews,
+  getReviewById,
+  createReview,
+  getReviewsReceived,
+  getReviewsGiven,
+  getUserRating,
+} from "./controller.js";
+import { user } from "../user/middleware";
 
 const router = express.Router();
 
 // GET /reviews - Get all reviews
-router.get('/', getAllReviews);
+router.get("/", getAllReviews);
 
 // GET /reviews/:id - Get a review by its ID
-router.get('/:id', getReviewById);
+router.get("/:id", getReviewById);
 
 // POST /reviews - Create a new review
-router.post('/', createReview); 
+router.post("/", user(), createReview);
 
 // GET /reviews/received/:id - Get all reviews received by a user
-router.get('/received/:id', getReviewsReceived);
+router.get("/received/:id", getReviewsReceived);
 
 // GET /reviews/given/:id - Get all reviews given by a user
-router.get('/given/:id', getReviewsGiven);
+router.get("/given/:id", getReviewsGiven);
 
 // GET /reviews/rating/:id - Get the rating of a user
-router.get('/rating/:id', getUserRating);
+router.get("/rating/:id", getUserRating);
 
 export default router;
